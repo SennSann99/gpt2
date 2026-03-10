c1119cae25a48a1a8f1faf97850839694a93b0a5
sen <cheongeewon@gmail.com>
2026-03-10 16:47:11 +0900

    revise main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from gpt2.train import main
-from gpt2.generate import generate
+from gpt2.train import main as train_main
+from gpt2.generate import main as generate_main
 
 if __name__ == "__main__":
-    main()
-    generate()
+    train_main()
+    generate_main()
